ecae9b7b93bfc15f25597337ebb06c68acdf35e9
AWSMProdigy <StraightAKyle@gmail.com>
2021-11-24 10:51:03 -0500

    Links to projects now working

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -8,32 +8,32 @@ function Portfolio() {
     {
       name: 'reel-cinema',
       description: 'Theater website made using HandlebarJs and Express',
-      link: "https://github.com",
-      repo: "https://github.com"
+      link: "https://intense-lake-77911.herokuapp.com/",
+      repo: "https://github.com/AWSMProdigy/Theater-website"
     },
     {
       name: 'Daily-Felon',
       description: 'Comical Newspaper website',
-      link: "https://github.com",
-      repo: "https://github.com"
+      link: "https://jamesstitzel.github.io/The-Daily-Felon/",
+      repo: "https://github.com/JamesStitzel/The-Daily-Felon"
     },
     {
       name: 'Landing-Page',
       description: 'Intro to UCF Bootcamp',
-      link: "https://github.com",
-      repo: "https://github.com"
+      link: "https://awsmprodigy.github.io/Landing-page/",
+      repo: "https://github.com/AWSMProdigy/Landing-page"
     },
     {
       name: 'Refactor',
       description: 'Debugging',
-      link: "https://github.com",
-      repo: "https://github.com"
+      link: "https://awsmprodigy.github.io/Code-Refactoring/",
+      repo: "https://github.com/AWSMProdigy/Code-Refactoring"
     },
     {
       name: 'RPS',
       description: 'Javascript magic',
-      link: "https://github.com",
-      repo: "https://github.com"
+      link: "https://awsmprodigy.github.io/RockPaperScissors/",
+      repo: "https://github.com/AWSMProdigy/RockPaperScissors"
     },
   ]);
 
