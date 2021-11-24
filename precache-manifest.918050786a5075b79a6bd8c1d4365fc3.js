47d79ad13774bb03baf4a25b664f10042acd7c61
AWSMProdigy <StraightAKyle@gmail.com>
2021-11-23 21:15:10 -0500

    Updates

---
+++ precache-manifest.918050786a5075b79a6bd8c1d4365fc3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da09360b1fceb9dc23bfb30475d3d4ee",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "895401c9e62016d72626",
+    "url": "/React-Portfolio/static/css/main.4ebfe0cc.chunk.css"
+  },
+  {
+    "revision": "ec7dea27f2d267470dff",
+    "url": "/React-Portfolio/static/js/2.102548e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Portfolio/static/js/2.102548e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "895401c9e62016d72626",
+    "url": "/React-Portfolio/static/js/main.e0dac79e.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  },
+  {
+    "revision": "df887f9feef1084f9366453b9527819e",
+    "url": "/React-Portfolio/static/media/Daily-Felon.df887f9f.jpg"
+  },
+  {
+    "revision": "02b9c25c1dce8c51433b3db195bb85aa",
+    "url": "/React-Portfolio/static/media/Landing-Page.02b9c25c.jpg"
+  },
+  {
+    "revision": "939ab7643c8de4bcf2fb9627791fa96a",
+    "url": "/React-Portfolio/static/media/RPS.939ab764.jpg"
+  },
+  {
+    "revision": "6e85167b7f9abf6a391a747ca05479e1",
+    "url": "/React-Portfolio/static/media/Refactor.6e85167b.jpg"
+  },
+  {
+    "revision": "a19f2b67a0540f784bf091db8e36c82a",
+    "url": "/React-Portfolio/static/media/calculator.a19f2b67.jpg"
+  },
+  {
+    "revision": "402a471b5aad190a33b649f1368f5de1",
+    "url": "/React-Portfolio/static/media/cover-image.402a471b.jpg"
+  },
+  {
+    "revision": "653c2b3d29f66a3c6e957518f3bd16f0",
+    "url": "/React-Portfolio/static/media/led-wall.653c2b3d.jpg"
+  },
+  {
+    "revision": "ed494730868bd902e0ee98f1df1877e2",
+    "url": "/React-Portfolio/static/media/pastel-puzzels.ed494730.jpg"
+  },
+  {
+    "revision": "8f0e3437d5b6fcfe3f270680171f9936",
+    "url": "/React-Portfolio/static/media/reel-cinema.8f0e3437.jpg"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/React-Portfolio/static/media/run-buddy.938f8fe0.jpg"
+  },
+  {
+    "revision": "12557c9b108cf5e5347f8d838014b31e",
+    "url": "/React-Portfolio/static/media/surf-report.12557c9b.jpg"
+  }
+]);
